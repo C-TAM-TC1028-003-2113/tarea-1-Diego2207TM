1676b9eb5137b991798e99221d261fdd6a9205ff
Diego Armando <A01412512@itesm.mx>
2021-09-01 13:00:14 -0500

    Entregado

--- assignments/10Videojuego/src/exercise.py
+++ assignments/10Videojuego/src/exercise.py
@@ -1,6 +1,6 @@
 def main():
-    a = float(input("Dame la cantidad de juegos nuevos: "))
-    b = float(input("Dame la cantidad de juegos usados: "))
+    a = int(input("Dame la cantidad de juegos nuevos: "))
+    b = int(input("Dame la cantidad de juegos usados: "))
     c = (a*1000)+(b*350)
     print("El total de la compra es:",c)
 
