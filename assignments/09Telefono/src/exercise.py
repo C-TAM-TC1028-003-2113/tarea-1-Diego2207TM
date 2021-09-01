02ec2c632a74020daafa61cf2f87d09ad422418b
Diego Armando <A01412512@itesm.mx>
2021-09-01 12:55:43 -0500

    Entregado

--- assignments/09Telefono/src/exercise.py
+++ assignments/09Telefono/src/exercise.py
@@ -1,8 +1,8 @@
 def main():
   import math
-  m = float(input("Dame el número de mensajes:"))
-  i = float(input("Dame el número de megas:"))
-  t = float(input("Dame el número de minutos:"))
+  m = float(input("Dame el número de mensajes: "))
+  i = float(input("Dame el número de megas: "))
+  t = float(input("Dame el número de minutos: "))
 
   precio = float(m*0.8)+(i*0.8)+(t*0.8)
   print("El costo mensual es:",precio)
